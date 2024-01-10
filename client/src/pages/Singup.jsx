cc59c484cc55ea367ae94a864e9c45f5d404879c
Mahadi9990 <mdshafayat9999@gmail.com>
2024-01-10 14:29:19 +0600

    set const error,loading,and set if ,then,catch,useNavigate and naviaget to sing in page

--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link ,useNavigate} from 'react-router-dom'
 
 export default function Singup() {
   const [formData,setFormData] =useState({})
+  const [error ,setError] =useState(null);
+  const [loading ,setLoading] =useState(false);
+  const navigate =useNavigate();
   const handleChange = (e)=>{
     setFormData({
       ...formData,
@@ -11,18 +14,28 @@ export default function Singup() {
   };
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    const res =await fetch('/user/outh',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body:JSON.stringify(formData),
-    });
-    const data =await res.json();
-    console.log(data);
-    
+    setLoading(true);
+    try {
+      const res =await fetch('/user/outh',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData),
+      });
+      const data =await res.json();
+      if(data.success === false){
+        setError(data.message);
+        setLoading(false);
+        return
+      }
+      setLoading(false);
+      navigate('/sing-in')
+    } catch (error) {
+      setLoading(false)
+      setError(error.message);
+    }
   }
-  console.log(formData);
   return (
     <>
       <div className='justify-center' id='main'>
@@ -31,9 +44,10 @@ export default function Singup() {
          <input type="text" onChange={handleChange}placeholder='User name' className='border rounded-lg p-3' id='userName'/>
          <input type="email" onChange={handleChange}placeholder='email' className='border rounded-lg p-3' id='email'/>
          <input type="password" onChange={handleChange}placeholder='password' className='border rounded-lg p-3' id='password'/>
-         <button type="submit" className='bg-blue-700'>Singup</button>
+         <button disabled={loading} type="submit" className='bg-blue-700'>{loading ? 'Loading...': 'sing up'}</button>
         </form>
         <p>Have a Acount <span className='text-blue-500 underline'><Link to="/sing-in">SingIn</Link></span></p>
+        {error && <p className='text-red-500 m-3'>{error}</p>}
       </div>
     </>
   )
