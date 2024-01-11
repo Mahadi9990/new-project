009f13e90fa0af8915fd45d0497d509160371747
Mahadi9990 <mdshafayat9999@gmail.com>
2024-01-12 00:06:54 +0600

    create redux persist to save after page loading ,creat rootReducer ,comdineReduce,persistedReducer in persistReducer,persistconfig,export persistor in persistStore and go to main.jsx file and create

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './app/store.js'
-import { Provider } from 'react-redux'
+import {persistor, store} from './app/store.js'
+import { Provider  } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>S
