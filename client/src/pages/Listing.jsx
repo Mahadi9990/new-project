a76df5e5642d360fd0990de505f57ba9859fe8ac
Mahadi9990 <mdshafayat9999@gmail.com>
2024-02-14 15:47:45 +0600

    complet listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -37,18 +37,42 @@ export default function Listing() {
       {error && <p className='text-center text-2xl my-7'>something is wrond</p>}
       {loading && <p className='text-center text-2xl my-7'>Loading...</p>}
       {listing && !error && !loading &&
-        <>
+        <div className='p-1'>
           <Swiper navigation>
             {listing.image.map((url) =>
               (
               <SwiperSlide key={url}>
-                <div style={{background:`url(${url}) center no-repeat`,backgroundSize:'cover'}} className='h-[550px]'></div>
+                <div style={{
+                  background:`url(${url}) center no-repeat`,
+                  backgroundSize:'cover',
+                  backgroundImage:'center'
+                }} className='h-[500px] '></div>
               </SwiperSlide>
               )
               )}
           </Swiper>
-        </>
+          <p className='text-2xl font-semibold uppercase my-2'>{listing.title} - $ {listing.market}{listing.type === 'rent'? <span className='lowercase'>/month</span>:' '}</p>
+          <p className='text-1xl font-semibold my-2'>loaction: {listing.address}</p>
+          <div className='flex gap-4'>
+            <div className='rounded-lg left w-full max-w-[200px] bg-red-500 p-2 text-center text-2xl font-semibold'>{listing.type === 'rent'? 'For Rent':'For sele'}</div>
+            {listing.offer && 
+                        <div className='rounded-lg right w-full max-w-[200px] bg-green-500 p-2 text-center text-2xl font-semibold'>
+                         Discount-$ {+listing.market - +listing.discount}
+                          </div>
+
+            }
+          </div>
+          <p className='lowercase my-2'><span className=' text-center text-1xl font-semibold uppercase'>Discription</span> - {listing.textArea}</p>
+          <ul className='font-semibold flex flex-row justify-around gap-1 flex-wrap text-green-600 text-1xl'>
+            <li>Bed: {listing.badroom}</li>
+            <li>Bath: {listing.bathroom}</li>
+            {listing.parking && <li>Parking</li>}
+            {listing.furnished && <li>Furnished</li>}
+          </ul>
+        </div>
       }
     </main>
   )
 }
+
+
