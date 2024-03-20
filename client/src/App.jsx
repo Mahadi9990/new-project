f6a22ed2e06ae880380e140393c85fc65112cc9a
Mahadi9990 <mdshafayat9999@gmail.com>
2024-03-20 16:09:50 +0600

    final update 01

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Profile from './pages/Profile';
 import Singin from './pages/Singin';
 import Singup from './pages/Singup';
 import CreateListing from './pages/CreateListing';
-import UpdataList from './pages/UpdateList';
+import UpdateList from './pages/UpdateList';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import Listing from './pages/Listing';
@@ -24,7 +24,7 @@ export default function App() {
         <Route element={<PrivateRoute/>}>
           <Route path={'/profile'} element={<Profile/>}/>
           <Route path={'/create-listing'} element={<CreateListing/>}/>
-          <Route path={'/update/listing/:listingId'} element={<UpdataList/>}/>
+          <Route path={'/update/listing/:listingId'} element={<UpdateList/>}/>
         </Route>
         <Route path={'/sing-in'} element={<Singin/>}/>
         <Route path={'/sing-up'} element={<Singup/>}/>
